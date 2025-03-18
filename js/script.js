f9818e9335faef20b6591a1fef77323219c3b2d4
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-18 15:06:42 +0100

    [UPD] Add Fizz and Buzz

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
+//Variabili
 let num=parseInt(prompt("Inserisci un numero compreso tra 1 e 100"));
+let odd= 'Fizz';
+let even='Buzz';
+let oddEven=' ';
+//Corpo programma
+for(let i=1; i<=100; i++){
+    if(i % 3 == 0 ){
+         console.log(odd);
+    }
+
+    else if(i % 5 == 0){
+        console.log(even);
+    }
+       
+  
+}
 console.log(num);
